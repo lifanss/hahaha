cac044fbdc7518805528ebff4a959d6072cc4445
insist <1697794459@qq.com>
2020-04-16 22:06:15 +0800

    第六次上传了，

--- src/cn/blb/Hahahaha.java
+++ src/cn/blb/Hahahaha.java
@@ -4,11 +4,10 @@ public class Hahahaha {
 	public static void main(String[] args) {
 		System.out.print("hahahaha!!!");
 		System.out.print("balabalabala!!!");
-<<<<<<< HEAD
 		System.out.print("abaxi!!!");
-=======
 		System.out.print("bibilailai!!!");
->>>>>>> branch 'master' of https://github.com/lifanss/hahaha.git
+		System.out.print("aaaaaaaaaaaaaaaaaaa!!!");
+		
 		
 	}
 }

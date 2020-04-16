e5098c86772002af02eb11d3714ad8bc465e3cd1
insist <1697794459@qq.com>
2020-04-16 21:54:39 +0800

    第五次上传，您的代码有误

--- src/cn/blb/Hahahaha.java
+++ src/cn/blb/Hahahaha.java
@@ -4,6 +4,7 @@ public class Hahahaha {
 	public static void main(String[] args) {
 		System.out.print("hahahaha!!!");
 		System.out.print("balabalabala!!!");
+		System.out.print("abaxi!!!");
 		
 	}
 }
